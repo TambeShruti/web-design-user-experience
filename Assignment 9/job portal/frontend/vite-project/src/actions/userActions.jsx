8ce7cf3fe03c01acd794755a4756f6e2a5a5f666
Shruti Tambe <114512750+TambeShruti@users.noreply.github.com>
2024-04-11 17:55:56 -0400

    getAll endpoint update

--- Assignment 9/job portal/frontend/vite-project/src/actions/userActions.jsx
+++ Assignment 9/job portal/frontend/vite-project/src/actions/userActions.jsx
@@ -1,6 +1,11 @@
-import { LOGIN_SUCCESS } from "../constants/userConstants";
+import { LOGIN_SUCCESS, VIEW_USERS } from "../constants/userConstants";
 
 export const loginSuccess = (user) => ({
   type: LOGIN_SUCCESS,
   payload: { email: user.email },
+});
+
+export const viewUsers = (users) => ({
+  type: VIEW_USERS,
+  payload: users,
 });
--- Assignment 9/job portal/frontend/vite-project/src/constants/userConstants.jsx
+++ Assignment 9/job portal/frontend/vite-project/src/constants/userConstants.jsx
@@ -1,1 +1,2 @@
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const VIEW_USERS = "VIEW_USERS";
